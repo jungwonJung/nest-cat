702b934432874cc07cddb13ece629a38a03dcb72
JUNGganzi <wjdwjd1501@gmail.com>
2021-08-24 22:38:46 +0900

    몽고DB 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { LoggerMiddleware } from 'common/middlewares/logger.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    CatsModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true, // 인덱싱을 하겠다
+      useFindAndModify: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
